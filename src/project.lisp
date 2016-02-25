4070b0697ea052a17dff207b14cf20bdbbb84895
Kevin Mas Ruiz <masruizkevin@gmail.com>
2016-02-25 23:59:48 +0100

    project ontology

---
+++ src/project.lisp
@@ -0,0 +1,76 @@
+#|
+This file is part of clpom project object model
+(c) 2016 Kevin Mas <masruizkevin@gmail.com>
+See LICENSE for more information
+|#
+
+(in-package #:clpom)
+
+;; project generics
+
+(defgeneric get-extra (project key))
+(defgeneric put-extra (project key value))
+(defgeneric add-task  (project task unit-of-work))
+(defgeneric add-task-dependency (project task dependency))
+(defgeneric run-project-task (project task))
+(defgeneric register-plugin (project plugin))
+
+(defclass project ()
+  ((name
+    :reader name
+    :type string
+    :initarg :name)
+   (extras
+    :accessor extras
+    :reader extras
+    :type list
+    :initarg :extras
+    :initform (list))
+   (tasks
+    :accessor tasks
+    :reader tasks
+    :type list
+    :initarg :tasks
+    :initform (list))))
+
+(defun make-project (name)
+  (make-instance 'project :name name))
+
+(defmethod get-extra ((project project) (key symbol))
+  (getf (extras project) key))
+
+(defmethod put-extra ((project project) (key symbol) value)
+  (setf (getf (extras project) key) value))
+
+(defmethod add-task ((project project) (task string) (unit-of-work function))
+  (flet ((task-name-matches (task-ref)
+	   (string= (name task-ref) task)))
+    (let ((task-ref (find-if #'task-name-matches (tasks project))))
+      (cond
+	((null task-ref)
+	 (setq task-ref (make-task task))
+	 (push task-ref (tasks project))
+	 (push-step-back task-ref (make-task-step unit-of-work)))
+	(t
+	 (push-step-back task-ref (make-task-step unit-of-work)))))))
+
+(defmethod add-task-dependency ((project project) (task string) (dependency string))
+  (flet ((task-name-matches (name)
+	   (lambda (task-ref) (string= name (name task-ref)))))
+    (let ((task-ref (find-if (task-name-matches task) (tasks project)))
+	  (dep-ref (find-if (task-name-matches dependency) (tasks project))))
+      (when (null task-ref)
+	(setq task-ref (make-task task))
+	(push task-ref (tasks project)))
+      (when (null dep-ref)
+	(setq dep-ref (make-task dependency))
+	(push dep-ref (tasks project)))
+      (task-depends-on task-ref dep-ref))))
+
+(defmethod run-project-task ((project project) (task string))
+  (flet ((task-name-matches (task-ref)
+	   (string= (name task-ref) task)))
+    (run-task (find-if #'task-name-matches (tasks project)))))
+
+(defmethod register-plugin ((project project) (plugin function))
+  (apply plugin (list project)))
